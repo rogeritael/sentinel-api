d20f3d41f07f2a1441bf601ad8c2769435f32b92
rogeritael <rogeritael.contato@gmail.com>
2024-01-22 01:08:35 -0300

    configurando as libs de scrap

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
+const cheerio = require('cheerio')
+const UserAgent = require('user-agents')
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('API funcionando')
+app.get('/', async(req, res) => {
+    const urlalvo = 'https://www.xbox.com/pt-BR/games/store/palworld-game-preview/9NKV34XDW014/0010/B1WB0HW5PVHN' 
+
+    const userAgent = await new UserAgent();
+    const { data } = await axios.get(urlalvo, {
+        'User-Agent': userAgent.toString()
+    })
+
+    const $ = cheerio.load(data)
+    let results = []
+    let price = await $('.Price-module__boldText___vmNHu.Price-module__moreText___q5KoT').map((index, element) => {
+        results = [...results, $(element).text()]
+    })
+
+    res.json({'preco': results[0]})
 })
 
 app.listen({
