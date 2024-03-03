d06dc35177091406fa54923e6cf003ed6cce905c
rogeritael <rogeritael.contato@gmail.com>
2024-03-02 22:13:18 -0300

    [update] instalando cors para poder recuperar informações no frontend

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const ScrapController = require('./src/controllers/ScrapController')
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 app.get('/', ScrapController.findXboxPromotions)
 
